e65ee166b48d897be7d8f2bcc062dec50ab08790
Glen Geen <gptwins@users.noreply.github.com>
2025-07-09 10:17:44 -0500

    Version 1.0.0002
    Added file and directory exclusion functionality.  Updated the help page so the get-help ./purgeChurchFiles.ps1 -details provides more information.

--- PurgeChurchFiles.ps1
+++ PurgeChurchFiles.ps1
@@ -3,16 +3,18 @@
     A quick PowerShell script to purge files from a given directory.
 
     .DESCRIPTION
-    This script reads the given directory and then deletes the files that are older
+    This script reads the given directory and then deletes the files and folders that are older
     than the provided file age, default is 30 days.  It then reads the given directory for other
     sub-directories and removes any directory not modified in time period given in the file age.
 
-    I provided an array within the script for directory exclustions.  Include in the $MyExcludes
-    the directories you wish to skip during recursive remove process.
+    I provided an array within the script, $defaultExcludes, for files and directories to
+    always exclude. Include in the $MyExcludes parameter the additional files and directories you wish 
+    to skip during recursive remove process.
 
     .PARAMETER Path
-    The fully qualified or relative path name of a file or directory to be logged.  The path may either 
-    be a fully qualified path name, C:\users\glen\downloads or a relative path name, ~\downloads.
+    The fully qualified or relative path name of a directory to be purged.  The path 
+    may either be a fully qualified path name, C:\users\username\downloads\ or a relative path 
+    name, ~\downloads\.  Be sure to include the trailing slash.
 
     .PARAMETER MyExcludes
     The MyExcludes parameter is an arry of strings.  These are the directories to ignore so they are not 
@@ -34,18 +36,43 @@
     #Run the script with default settings; delete files and sub-folders in the defualt directory older than 30 days old.
 
     .EXAMPLE
-    PS> ./PurgeChurchfiles.ps1 -Path c:\users\Foo
-    #Purge the bsolute path of C:\users\Foo of files and folders older than 30 days.
-
+    PS C:\Users\username\src\ps1> .\PurgeChurchFiles.ps1 -Path C:\inetpub\wwwroot\ -FileAge 7 -WhatIf
+    What if: Performing the operation "Remove File" on target "C:\inetpub\wwwroot\Click-it.html".
+    What if: Performing the operation "Remove File" on target "C:\inetpub\wwwroot\favicon.ico".
+    What if: Performing the operation "Remove File" on target "C:\inetpub\wwwroot\ggeen-menu.css".
+    What if: Performing the operation "Remove File" on target "C:\inetpub\wwwroot\index.html".
+    What if: Performing the operation "Remove File" on target "C:\inetpub\wwwroot\MenuPlanner.html".
+    What if: Performing the operation "Remove File" on target "C:\inetpub\wwwroot\program.html".
+    What if: Performing the operation "Remove File" on target "C:\inetpub\wwwroot\resource.html".
+    What if: Performing the operation "Remove File" on target "C:\inetpub\wwwroot\todolist.html".
+    What if: Performing the operation "Remove File" on target "C:\inetpub\wwwroot\TrendBrand2023.css".
+    What if: Performing the operation "Remove Directory" on target "C:\inetpub\wwwroot\BlogShorts".
+    What if: Performing the operation "Remove Directory" on target "C:\inetpub\wwwroot\Commands".
+    What if: Performing the operation "Remove Directory" on target "C:\inetpub\wwwroot\gov".
+    What if: Performing the operation "Remove Directory" on target "C:\inetpub\wwwroot\images".
+    What if: Performing the operation "Remove Directory" on target "C:\inetpub\wwwroot\Recipes".
+    What if: Performing the operation "Remove Directory" on target "C:\inetpub\wwwroot\test".
+    PS C:\Users\username\src\ps1>
+    
     .EXAMPLE
     PS> ./PurgeChurchFiles.ps1 -Path ~\Downloads
     #Purge the relative path name and sub-directories older than 30 days.
 
     .EXAMPLE
-    PS> ./PurgeChurchFiles.ps1 -Path C:\Windows\Temp -MyExcludes "My Assets","Keep Folder" -FileAge 90
-    #Deletes all files and folders in C:\windows\Temp older than 90 days except the sub-directorires "My Assets" and "Keep Folder".  These
-    #sub-directories are skipped during the deletion process.
-
+    PS C:\Users\username\src\ps1> .\PurgeChurchFiles.ps1 -Path C:\inetpub\wwwroot\ -MyExcludes "*.css","*.ico" -FileAge 7 -WhatIf
+    What if: Performing the operation "Remove File" on target "C:\inetpub\wwwroot\Click-it.html".
+    What if: Performing the operation "Remove File" on target "C:\inetpub\wwwroot\index.html".
+    What if: Performing the operation "Remove File" on target "C:\inetpub\wwwroot\MenuPlanner.html".
+    What if: Performing the operation "Remove File" on target "C:\inetpub\wwwroot\program.html".
+    What if: Performing the operation "Remove File" on target "C:\inetpub\wwwroot\resource.html".
+    What if: Performing the operation "Remove File" on target "C:\inetpub\wwwroot\todolist.html".
+    What if: Performing the operation "Remove Directory" on target "C:\inetpub\wwwroot\BlockThisFolder".
+    What if: Performing the operation "Remove Directory" on target "C:\inetpub\wwwroot\BlogShorts".
+    What if: Performing the operation "Remove Directory" on target "C:\inetpub\wwwroot\Commands".
+    What if: Performing the operation "Remove Directory" on target "C:\inetpub\wwwroot\images".
+    What if: Performing the operation "Remove Directory" on target "C:\inetpub\wwwroot\Recipes".
+    What if: Performing the operation "Remove Directory" on target "C:\inetpub\wwwroot\test".
+PS C:\Users\username\src\ps1> 
     .NOTES
     Author: G D Geen
     Date-written: 3 Jul 2025
@@ -58,17 +85,21 @@ param (
     [switch] $WhatIf
 )
 
-$Version = "1.0.0001"
+Set-Variable Version -Option Constant -Value "1.0.0002"
+$defaultExlcudes = @("desktop.ini","BlockThisFolder")
+$allExcludes = @()
 $AnchorDate=(get-date).AddDays(-$FileAge) #get today's date and subtract the value of $FileAge.
 # $MyExcludes=@('gov','BlogShorts') #Array of directories to exclude from get-childitem collection.
 
+$allExcludes = $($defaultExlcudes; $MyExcludes)
+
 if($WhatIf) {
-  get-childitem -path $Path -file |where-object {$_.CreationTime -lt $AnchorDate} | remove-item -whatif
-  get-childitem -path $Path -exclude $MyExcludes -directory |where-object {$_.CreationTime -lt $AnchorDate} | remove-item -recurse -whatif
+  get-childitem -path $Path\* -exclude $allExcludes -file |where-object {$_.CreationTime -lt $AnchorDate} | remove-item -whatif
+  get-childitem -path $Path -exclude $allExcludes -directory |where-object {$_.CreationTime -lt $AnchorDate} | remove-item -recurse -whatif
 }
 else {
-    get-childitem -path $Path -file |where-object {$_.CreationTime -lt $AnchorDate}
-    get-childitem -path $Path -exclude $MyExcludes -directory |where-object {$_.CreationTime -lt $AnchorDate}
+  get-childitem -path $Path\* -exclude $allExcludes -file |where-object {$_.CreationTime -lt $AnchorDate} | remove-item
+  get-childitem -path $Path -exclude $allExcludes -directory |where-object {$_.CreationTime -lt $AnchorDate} | remove-item -recurse
 }
 
 #get directory names from the given path, exluding those I dont want delete, recursively remove the directory and its contents (-whatif ) 
