cd5e2d76ebf87374ce16c8c814ed9e005ef808f8
Glen Geen <gptwins@users.noreply.github.com>
2025-08-11 08:16:58 -0500

    Get-SEP-PolicyInfo.ps1, Version: 1.0.2025.213
    Retrieves the installation directory, server name/IP address, policy assigned, and the last time the policy was updated.
    
    PS > .\src\ps1\Get-SEP-PolicyInfo.ps1
    Agent Installation Directory: C:\Program Files (x86)\Trend Micro\Security Agent\
    Server Address: mvxd9q.manage.trendmicro.com
    Policy Name: GlenGeen-Lab
    Last Update Time: 2025-07-31 15:29:59

---
+++ Get-SEP-PolicyInfo.ps1
@@ -0,0 +1,58 @@
+<#
+    .SYNOPSIS
+    A quick PowerShell script to return the Trend Vision One Standard Endpoint Protection/Apex One agent install directory, server name/address, policy name applied, and the last time the policy was applied.
+
+    .DESCRIPTION
+    The script reads the Windows registry to determine of the Vision One Standard Endpoint Protection/Apex One agent is installed.  It returns the installation directory, the fully qualified server name or IP address of the server, The SEP/Apex One agent policy that is currently applied, and the last date/time the policy was applied to the agent.
+
+    .PARAMETER Version
+    There is only one available switch, that is to show the current version number of the script.
+
+    .EXAMPLE
+    PS C:\Users\gdgee\src\ps1> .\Get-SEP-PolicyInfo.ps1
+    Agent Installation Directory: C:\Program Files (x86)\Trend Micro\Security Agent\
+    Server Address: xxxxx.manage.trendmicro.com
+    Policy Name: GlenGeen-Lab
+    Last Update Time: 2025-07-31 15:29:59
+
+    .EXAMPLE
+    PS C:\Users\gdgee\src\ps1> .\Get-SEP-PolicyInfo.ps1 -Version
+    Get-SEP-PolicyInfo.ps1
+    Version: 1.0.2025.213
+
+.NOTES
+    Author: G D Geen
+    Date-written: 1 Aug 2025
+#>
+
+Param (
+    [switch] $Version
+)
+########################################################################
+# Configure script operation
+########################################################################
+$ErrorActionPreference = 'SilentlyContinue'
+
+########################################################################
+# Set variables
+########################################################################
+$ProgramName = $MyInvocation.MyCommand.Name
+$ProgramVersion = "1.0.2025.213"
+$AgentPath = ""
+$ServerAddr = ""
+
+if( $Version ) { 
+    Write-Host "$ProgramName`nVersion`: $ProgramVersion"
+    exit 0
+}
+
+$AgentPath =  (Get-ItemProperty -Path Registry::\HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\TrendMicro\PC-cillinNTCorp\CurrentVersion -Name "Application Path")."Application Path"
+$ServerAddr = (Get-ItemProperty -Path Registry::\HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\TrendMicro\PC-cillinNTCorp\CurrentVersion -Name "Server")."Server"
+
+if( $AgentPath -ne "") {
+"Agent Installation Directory: $AgentPath"
+"Server Address: $ServerAddr"
+"Policy Name: "+(Get-ItemProperty -Path Registry::\HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\TrendMicro\PC-cillinNTCorp\CurrentVersion\Misc.\ -name "CMAPolicy0_Name" |select-object CMAPolicy0_Name).CMAPolicy0_Name
+"Last Update Time: "+(Get-ItemProperty -Path Registry::\HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\TrendMicro\PC-cillinNTCorp\CurrentVersion\Misc.\ -name "CMAPolicy0_LastUpdateTime" |select-object CMAPolicy0_LastUpdateTime).CMAPolicy0_LastUpdateTime
+}
+esle { "Agent does not appear to be installed." }
