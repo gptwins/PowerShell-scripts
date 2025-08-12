d9ce18324ea0dd04b9b8ff50e327a1d89b1ae8e9
Glen Geen <gptwins@users.noreply.github.com>
2025-08-12 08:51:56 -0500

    Version: 1.1.2025.224
    Added in XDRinfo option that returns XDR Business ID, Client ID, and XDR region information.
    
        PS >.\Get-SEP-PolicyInfo.ps1 -XDRinfo
    
        Agent Installation Directory: C:\Program Files (x86)\Trend Micro\Security Agent\
        Server Address: xxxxx.manage.trendmicro.com
        Program Version: 14.0
        Program Build Number: 20114
        Policy Name: GlenGeen-Lab
        Last Update Time: 2025-07-31 15:29:59
        Vision One Buisness ID: xxxxxxxx-d53f-49f5-8be6-xxxxxxxxxxxx
        Vision One Device ID: xxxxxxxx-797a-4d81-b4c8-xxxxxxxxxxxx
        Vision One Region: prod-us1

--- Get-SEP-PolicyInfo.ps1
+++ Get-SEP-PolicyInfo.ps1
@@ -5,8 +5,11 @@
     .DESCRIPTION
     The script reads the Windows registry to determine of the Vision One Standard Endpoint Protection/Apex One agent is installed.  It returns the installation directory, the fully qualified server name or IP address of the server, the installed SEP/Apex One version and build number, the SEP/Apex One agent policy that is currently applied, and the last date/time the policy was applied to the agent.
 
+    .PARAMETER XDRinfo
+    Also include Trend Vision One Endpoint Sensor information with the Apex One/Trend Vision One Standard Endpoint Protection (SEP) information.
+
     .PARAMETER Version
-    There is only one available switch, that is to show the current version number of the script.
+    Show the current version number of the script.
 
     .EXAMPLE
     PS > .\Get-SEP-PolicyInfo.ps1
@@ -16,6 +19,18 @@
     Program Build Number: 20114
     Policy Name: GlenGeen-Lab
     Last Update Time: 2025-07-31 15:29:59
+
+    .EXAMPLE
+    PS C:\Users\gdgee\src\ps1> .\Get-SEP-PolicyInfo.ps1 -XDRinfo
+    Agent Installation Directory: C:\Program Files (x86)\Trend Micro\Security Agent\
+    Server Address: xxxxx.manage.trendmicro.com
+    Program Version: 14.0
+    Program Build Number: 20114
+    Policy Name: GlenGeen-Lab
+    Last Update Time: 2025-07-31 15:29:59
+    Vision One Buisness ID: xxxxxxxx-d53f-49f5-8be6-xxxxxxxxxxxx
+    Vision One Device ID: xxxxxxxx-797a-4d81-b4c8-xxxxxxxxxxxx
+    Vision One Region: prod-us1
 
     .EXAMPLE
     PS C:\Users\gdgee\src\ps1> .\Get-SEP-PolicyInfo.ps1 -Version
@@ -28,8 +43,23 @@
 #>
 
 Param (
-    [switch] $Version
+    [switch] $Version,
+    [switch] $XDRinfo
 )
+
+function get-XDRinfo() {
+    $XDRbusinessID = (Get-ItemProperty -Path Registry::\HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\TrendMicro\TMSecurityService -Name "business_id")."business_id"
+    $XDRdeviceID = (Get-ItemProperty -Path Registry::\HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\TrendMicro\TMSecurityService -Name "xdr_device_id")."xdr_device_id"
+    $XDRenv = (Get-ItemProperty -Path Registry::\HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\TrendMicro\TMSecurityService -Name "vom_env")."vom_env"
+
+    if( $XDRdeviceID -ne "" ) {
+    "Vision One Buisness ID: $XDRbusinessID"
+    "Vision One Device ID: $XDRdeviceID"
+    "Vision One Region: $XDRenv"
+    }
+    else { "Trend Vision One Endpoint Security does not appear to be installed."}
+}
+
 ########################################################################
 # Configure script operation
 ########################################################################
@@ -39,7 +69,7 @@ $ErrorActionPreference = 'SilentlyContinue'
 # Set variables
 ########################################################################
 $ProgramName = $MyInvocation.MyCommand.Name
-$ProgramVersion = "1.0.2025.223"
+$ProgramVersion = "1.1.2025.224"
 $AgentPath = ""
 $ServerAddr = ""
 
@@ -60,5 +90,6 @@ if( $AgentPath -ne "") {
 "Program Build Number: $BuildNum"
 "Policy Name: "+(Get-ItemProperty -Path Registry::\HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\TrendMicro\PC-cillinNTCorp\CurrentVersion\Misc.\ -name "CMAPolicy0_Name" |select-object CMAPolicy0_Name).CMAPolicy0_Name
 "Last Update Time: "+(Get-ItemProperty -Path Registry::\HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\TrendMicro\PC-cillinNTCorp\CurrentVersion\Misc.\ -name "CMAPolicy0_LastUpdateTime" |select-object CMAPolicy0_LastUpdateTime).CMAPolicy0_LastUpdateTime
+if( $XDRinfo ) { get-XDRinfo }
 }
 esle { "Agent does not appear to be installed." }
