38c35e364a605301cb31eb3cbaa6984b58913c5b
Glen Geen <gptwins@users.noreply.github.com>
2025-08-19 13:04:20 -0500

    Version: 1.1.25231
    Added option for a short build number consisting of Major build number, minor build number, two-digit year built, and three-digit day-of-year built.  The new short build looks like 1.1.25231.  Using the default long version number looks like 1.1.2025.231.
    
    Also changed the major and minor build number from int32 to string types.
    
    Example command;  .\Gen-ProgramVersion.ps1 -MajorVersion "1" -MinorVersion "1" -ShortBuild
    $ProgramName = $MyInvocation.MyCommand.Name
    $ProgramVersion = "1.1.25231"
    
     .\Gen-ProgramVersion.ps1 -MajorVersion "1" -MinorVersion "1" 
    $ProgramName = $MyInvocation.MyCommand.Name
    $ProgramVersion = "1.1.2025.231"

--- Gen-ProgramVersion.ps1
+++ Gen-ProgramVersion.ps1
@@ -1,23 +1,27 @@
 Param(
-    [int32] $Version,
-    [int32] $MinorVersion,
-    [int32] $YearBuilt,
-    [int32] $DayOfYear,
-    [switch] $ShowVersion
+    [string] $MajorVersion,
+    [string] $MinorVersion,
+    [string] $YearBuilt,
+    [string] $ShortYearBuilt,
+    [string] $DayOfYear,
+    [switch] $ShortBuild,
+    [switch] $Version
 )
 
-$myVersion = "1.0.2025.204"
+$ProgramVersion = "1.1.25231"
 $ProgramName = $MyInvocation.MyCommand.Name
 
-if( $ShowVersion) { 
-    Write-Host "$ProgramName`: $myVersion"
+if( $Version) { 
+    Write-Host "$ProgramName`: $ProgramVersion"
     exit 0
 }
 
-if( $Version -eq "") { $Version = Read-Host "Major Version Number: " }
-if( $MinorVersion -eq "") { $MinorVersion = Read-host "Minor Version Number: " }
+if( $MajorVersion -eq "") { $MajorVersion = Read-Host "Major Version Number" }
+if( $MinorVersion -eq "") { $MinorVersion = Read-host "Minor Version Number" }
 if( $YearBuilt -eq "") { $YearBuilt = get-date -UFormat "%Y" }
+if( $ShortYearBuilt -eq "") { $ShortYearBuilt = get-date -UFormat "%y" }
 if( $DayofYear -eq "") { $DayOfYear = (get-date).dayofyear }
 
-Write-Host "`"`$Version = $version.$minorversion.$yearbuilt.$dayofyear`'"
-Write-Host "`$Version = $version`n`$MinorVersion = $minorversion`n`$MajorBuild = $yearbuilt`n`$MinorBuild = $dayofyear"
+Write-host '$ProgramName = $MyInvocation.MyCommand.Name'
+if ( $ShortBuild ) { Write-Host "`$ProgramVersion = `"$majorversion.$minorversion.$shortyearbuilt$dayofyear`"" }
+else { Write-Host "`$ProgramVersion = `"$majorversion.$minorversion.$yearbuilt.$dayofyear`"" }
