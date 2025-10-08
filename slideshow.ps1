1ed2cbb47d4fa92572844a109e16c1dbb3fa2464
Glen Geen <gptwins@users.noreply.github.com>
2025-10-08 14:05:43 -0500

    Version: 1.0.25281
    Added new option, -Sleeps, Sleep Seconds.  This a the number of seconds between taking down the last image and starting the new image display.

--- slideshow.ps1
+++ slideshow.ps1
@@ -1,17 +1,25 @@
 param (
 	[string]$Path = $env:homedrive+$env:homepath,
 	[int32]$Seconds = 600,
+	[int32]$Sleeps = 3,
 	[string]$Logfile = ""
 	)
 
 $ErrorActionPreference = 'SilentlyContinue'
 $pics = Get-ChildItem $Path/*.png,$Path/*.jpg,$Path/*.jpeg,$Path/*.gif,$Path/*.jiff -name
 $sizeOfPics = @($pics).Length
+$ProgramName = $MyInvocation.MyCommand.Name
+$ProgramVersion = "1.0.25281"
+
+write-host("Program Name: $ProgramName`nProgram Version: $ProgramVersion")
 write-host("Total number of slides: $sizeOfPics")
 
-for( $i=0; $i -lt $sizeOfPics; $i++){
+for( $i=0; $i -lt $sizeOfPics; $i++) 
+{ 
 	$TimeStamp = (Get-Date).toString("yyyy/MM/dd, HH:mm:ss")
 	$photo = Get-Random $pics
+
+
 	<# if ( (Test-path -Path $photo -PathType Leaf) ) { 
 		write-host($photo)		
 	} #>
@@ -23,26 +31,21 @@ for( $i=0; $i -lt $sizeOfPics; $i++){
 	start-sleep -seconds $Seconds
 
 	$OSver = (Get-WmiObject -class Win32_OperatingSystem).Caption
-	switch ($OSver) {
+    switch ($OSver) {
 		"Microsoft Windows 11" {
 			stop-process -name "PhotosApp" 
-			start-sleep -seconds 3
 		}
 		"Microsoft Windows 11 Pro" {
 			stop-process -name "photos"
-			start-sleep -seconds 3
 		}
 		"Microsoft Windows 11 Enterprise" {
 			stop-process -name "photos"
-			start-sleep -seconds 3
 		}
 		* {
 			stop-process -name "Microsoft.Photos"
-			start-sleep -seconds 3
 		}
 	}
-	
-	
+	start-sleep -seconds $Sleeps
 }
 
 <# 
